71078b5d480e0e329e2119e10c93467fea904b22
Raquel <raquelbfalves14@gmail.com>
2025-11-27 13:06:20 +0000

    ADD: print_age

--- aux_func.h
+++ aux_func.h
@@ -25,6 +25,7 @@ Cell **copy_matrix(Cell **src, int rows, int cols);
 //   rows – number of rows in the matrix
 //   cols – number of columns in the matrix
 void print_ecosystem(Cell **M, int rows, int cols);
+void print_age(Cell **M, int rows, int cols);
 
 
 #endif // AUX_FUNC_H
